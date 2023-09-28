3bab445802208291951aca8cc8494917328fbaca
keshav019 <keshavkumaryadav2000@gmail.com>
2023-09-28 19:48:29 +0530

    notification-service added

---
+++ notification-service/src/test/java/com/myLiabray/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myLiabray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
