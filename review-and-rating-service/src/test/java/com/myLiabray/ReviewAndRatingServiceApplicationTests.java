18a56f80ba25e2ad136bb37813264e2cf4dbf7d0
keshav019 <keshavkumaryadav2000@gmail.com>
2023-09-28 13:47:56 +0530

    review and rating service added

---
+++ review-and-rating-service/src/test/java/com/myLiabray/ReviewAndRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myLiabray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewAndRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
